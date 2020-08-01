4261fda2c16be8a2d9fd1d297d0c2dcf4d25ffaf
flaberd <flaberd@ukr.net>
2020-08-01 10:47:36 +0300

    Налаштування мови

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,6 +11,8 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
+    'sourceLanguage' => 'uk',
+    'language' => 'uk',
     'modules' => [],
     'components' => [
         'request' => [
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -11,6 +11,8 @@ return [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
+    'sourceLanguage' => 'uk',
+    'language' => 'uk',
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
